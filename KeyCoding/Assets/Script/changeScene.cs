ad7188e7bb754d815484181253a81f3c2e3f5c12
deng-ui <enqn_k@naver.com>
2018-10-27 14:46:29 +0900

    First, Second Scene

---
+++ KeyCoding/Assets/Script/changeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GotoSecond(){
+        SceneManager.LoadScene("SecondScene");
+    }
+
+    public void GoHome()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+}
---
+++ KeyCoding/Assets/Script/inputScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class inputScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private string textField = "";
+
+    private void OnGUI()
+    {
+        textField = GUI.TextArea(new Rect(500, 11, 275, 295), textField);
+
+        //GUI.Label(new Rect(100))
+    }
+}
