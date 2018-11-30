f677d704d71665b11b362a2ee08c08550ecde826
deng-ui <enqn_k@naver.com>
2018-11-30 12:49:25 +0900

    add design

---
+++ KeyCoding/Assets/Script/Spike.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spike : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        float SpikeX = moveSpeed * Time.deltaTime;
+        transform.Translate(-SpikeX, 0, 0);
+    }
+}
---
+++ KeyCoding/Assets/Script/mosterAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mosterAttack : MonoBehaviour {
+    public GameObject Spike;
+    public bool canShoot = true;
+    public float shootDelay;
+    float shootTimer = 0;
+
+    void LateUpdate() 
+    {
+        if (shootTimer > shootDelay) 
+        {
+            Instantiate(Spike, transform.position, Quaternion.identity);
+            shootTimer = 0;
+        }
+        shootTimer += Time.deltaTime;
+    }
+
+}
